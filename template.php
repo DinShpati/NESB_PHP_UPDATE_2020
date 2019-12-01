7245cf07dd8498465c984c9385182b66b6c6eeb6
Din Spataj <proendergamer03@gmail.com>
2019-12-01 16:14:12 +0300

    HTML TO PHP FOOTER
    I did:
    index.php
    insectrepell.php
    lipbalm.php
    naturaloil.php
    shamp_cond.php
    template.php
    testimonials.php
    wheretobp.php
    whyrab.php
    whysb.php

--- template.php
+++ template.php
@@ -1,6 +1,7 @@
 <?php
 
 include 'header.php';
+include 'footer.php';
 
 ?>
 <!doctype html>
@@ -70,24 +71,7 @@ include 'header.php';
            <!-- content goes here -->
            
            <!-- footer-->
-           <div class="row body">
-                   <div class="col-sm-5">
-                      
-                       <a href="#"><img src="img/logo-facebook.png" class="icons" alt="Folllow us on Facebook"/></a>
-                       <a href="#"><img src="img/twitter-logo.png" class="icons" alt="Folllow us on Twitter"/></a>
-                       <a href="#"><img src="img/icon2.png" class="" alt="Network" width="22px" style="margin-left: 5px;"/></a>
-                   </div>
-                   <div class="col-sm-7">
-                       <div class="quote">
-                           <blockquote>
-                               <span style="font-size: 18px; color:white;"><span style="font-size: 60px; color:forestgreen;font-family: happy-monkey">“
-                               </span> <i class="slogan">A name you can trust at a price you can afford! </i><span style="font-size: 60px; color:forestgreen;font-family: happy-monkey">”
-                               </span></span>
-                           </blockquote>
-                           <p style="color: white;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Designs By <span  class="afrim">Afrim Shpati</span></p>
-                       </div>
-                   </div>
-            </div>
+           <?php mainFooter();?>
        <!-- end footer-->
        
        
