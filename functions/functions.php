0741dacedb5fe1c41dacca442342b96a07cc045f
Din Spataj <proendergamer03@gmail.com>
2020-02-14 01:49:11 +0300

    Back

--- functions/functions.php
+++ functions/functions.php
@@ -1,8 +1,8 @@
 <?php
 
     $dbhost = "localhost";
-    $dbuser = "u729631644_dinshpati";
-    $dbpass = 'Z3tT57cW_$_!_*';
+    $dbuser = "root";
+    $dbpass = "";
     $db = "nesb";
     $con = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n". $con -> error);
 
