67b6127c86454fc37305d753c51cdbaf0470da1c
Din Spataj <proendergamer03@gmail.com>
2020-02-14 00:31:22 +0300

    MYSQL CONN UPDATE

--- functions/functions.php
+++ functions/functions.php
@@ -2,7 +2,7 @@
 
     $dbhost = "localhost";
     $dbuser = "u729631644_dinshpati";
-    $dbpass = "Z3tT57cW_$_!_*";
+    $dbpass = 'Z3tT57cW_$_!_*';
     $db = "nesb";
     $con = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n". $con -> error);
 
--- nesb/shpati/admin/includes/db.php
+++ nesb/shpati/admin/includes/db.php
@@ -4,7 +4,7 @@ global $con;
 
 $dbhost = "localhost";
 $dbuser = "u729631644_dinshpati";
-$dbpass = "Z3tT57cW_$_!_*";
+$dbpass = 'Z3tT57cW_$_!_*';
 $db = "nesb";
 $con = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n". $con -> error);
 
