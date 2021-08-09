8dc0fa2e5be7b78786f7f36155957feb272adbc8
Din Spataj <proendergamer03@gmail.com>
2021-08-09 14:32:41 +0300

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -2,9 +2,8 @@
 
     $dbhost = "u729631644_nesb";
     $dbuser = "u729631644_dinshpati";
-    $dbpass = "Z3tT57cW_$_!_*";
-    $db = "nesb";
-    $con = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n". $con -> error);
+    $dbpass = 'Z3tT57cW_$_!_*';
+    $con = new mysqli($dbhost, $dbuser, $dbpass) or die("Connect failed: %s\n". $con -> error);
 
 //Getting the Users Ip Address
 function getUserIpAddr(){
